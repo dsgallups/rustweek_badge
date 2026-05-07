88cd6b5ea4a20c68f0b4b36685929c03c2a5fe1a
dsgallups <dsgallups@protonmail.com>
2026-05-07 14:42:13 -0400

    fix: remove target

--- testembassy/src/bluetooth/mod.rs
+++ testembassy/src/bluetooth/mod.rs
@@ -1,11 +1,19 @@
 mod server;
 
-use crate::{CONNECTIONS_MAX, L2CAP_CHANNELS_MAX, consts::BLUETOOTH_MAC_ADDRESS};
-use bt_hci::controller::ExternalController;
+use crate::{
+    CONNECTIONS_MAX, L2CAP_CHANNELS_MAX,
+    bluetooth::server::Server,
+    consts::{BLUETOOTH_DEVICE_ADDRESS, DEVICE_NAME},
+};
+use bt_hci::{controller::ExternalController, uuid::appearance};
 use embassy_executor::Spawner;
 use esp_hal::peripherals::BT;
 use esp_radio::ble::controller::BleConnector;
-use trouble_host::{Address, Host, HostResources, prelude::DefaultPacketPool};
+use trouble_host::{
+    Address, Host, HostResources,
+    gap::{GapConfig, PeripheralConfig},
+    prelude::DefaultPacketPool,
+};
 
 pub fn init(spawner: &Spawner, bluetooth: BT<'static>) {
     // BLE controller stuff. This is the the HCI "Host-Controller Interface" lower half.
@@ -16,13 +24,19 @@ pub fn init(spawner: &Spawner, bluetooth: BT<'static>) {
     let mut resources: HostResources<DefaultPacketPool, CONNECTIONS_MAX, L2CAP_CHANNELS_MAX> =
         HostResources::new();
     let stack = trouble_host::new(ble_controller, &mut resources)
-        .set_random_address(Address::random(BLUETOOTH_MAC_ADDRESS));
+        .set_random_address(Address::random(BLUETOOTH_DEVICE_ADDRESS));
     let Host {
         mut peripheral,
         runner,
         central,
         ..
     } = stack.build();
+
+    let server = Server::new_with_config(GapConfig::Peripheral(PeripheralConfig {
+        name: DEVICE_NAME,
+        appearance: &appearance::UNKNOWN,
+    }))
+    .unwrap();
 
     // GATT (Generic Attribute File) server. This defines how BLE devices exchange data once
     // they're connected. It holds the data and exposes it to other devices.
@@ -40,7 +54,7 @@ pub async fn listen_to_bluetooth(bluetooth: BT<'static>) {
         HostResources::new();
 
     let stack = trouble_host::new(ble_controller, &mut resources)
-        .set_random_address(Address::random(BLUETOOTH_MAC_ADDRESS));
+        .set_random_address(Address::random(BLUETOOTH_DEVICE_ADDRESS));
 
     let host = stack.build();
 
--- testembassy/src/consts.rs
+++ testembassy/src/consts.rs
@@ -1,11 +1,16 @@
 use trouble_host::attribute::Uuid;
 
-/// Set your own for building purposes. we should have different ones
-pub const BLUETOOTH_MAC_ADDRESS: [u8; 6] = [0xff, 0x8f, 0x1a, 0x05, 0xe4, 0xff];
+/// Set your own for building purposes. we should have different ones.
+///
+/// This is analogous to a MAC address.
+pub const BLUETOOTH_DEVICE_ADDRESS: [u8; 6] = [0xff, 0x8f, 0x1a, 0x05, 0xe4, 0xff];
 pub const DEVICE_NAME: &str = "nameyourbadge";
 
 /// Dont change these
 pub const CONNECTIONS_MAX: usize = 1;
+
+/// The Logical Link Control and Adaptation Protocol (L2CAP) provides the data transport layer for BLE.
+/// GATT and other protocols run on top of L2CAP.
 pub const L2CAP_CHANNELS_MAX: usize = 2;
 
 pub const SERVICE_UUID: u128 = 12897126749781238;
