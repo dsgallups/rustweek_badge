5f62a4acb4abbec1bd1f90ff6eca30e998aceebd
dsgallups <dsgallups@protonmail.com>
2026-05-07 15:04:28 -0400

    make bluetooth task static

--- testembassy/src/bluetooth/mod.rs
+++ testembassy/src/bluetooth/mod.rs
@@ -6,37 +6,69 @@ use crate::{
     consts::{BLUETOOTH_DEVICE_ADDRESS, DEVICE_NAME},
 };
 use bt_hci::{controller::ExternalController, uuid::appearance};
+use defmt::panic;
 use embassy_executor::Spawner;
+use embassy_futures::join::join;
 use esp_hal::peripherals::BT;
 use esp_radio::ble::controller::BleConnector;
+use static_cell::StaticCell;
 use trouble_host::{
-    Address, Host, HostResources,
+    Address, Host, HostResources, Stack,
     gap::{GapConfig, PeripheralConfig},
-    prelude::DefaultPacketPool,
+    peripheral::Peripheral,
+    prelude::{DefaultPacketPool, Runner},
 };
 
-pub fn init(spawner: &Spawner, bluetooth: BT<'static>) {
+type BleController = ExternalController<BleConnector<'static>, 20>;
+
+pub async fn init(spawner: &Spawner, bluetooth: BT<'static>) {
     // BLE controller stuff. This is the the HCI "Host-Controller Interface" lower half.
     let transport = BleConnector::new(bluetooth, Default::default()).unwrap();
     let ble_controller = ExternalController::<_, 20>::new(transport);
 
+    static RESOURCES: StaticCell<
+        HostResources<DefaultPacketPool, CONNECTIONS_MAX, L2CAP_CHANNELS_MAX>,
+    > = StaticCell::new();
+    static STACK: StaticCell<Stack<'static, BleController, DefaultPacketPool>> = StaticCell::new();
+    static SERVER: StaticCell<Server<'static>> = StaticCell::new();
+
     // Trouble host stack
-    let mut resources: HostResources<DefaultPacketPool, CONNECTIONS_MAX, L2CAP_CHANNELS_MAX> =
-        HostResources::new();
-    let stack = trouble_host::new(ble_controller, &mut resources)
-        .set_random_address(Address::random(BLUETOOTH_DEVICE_ADDRESS));
+    let resources = RESOURCES.init(HostResources::new());
+    let stack = STACK.init(
+        trouble_host::new(ble_controller, resources)
+            .set_random_address(Address::random(BLUETOOTH_DEVICE_ADDRESS)),
+    );
+
     let Host {
         mut peripheral,
-        runner,
-        central,
+        mut runner,
         ..
     } = stack.build();
 
-    let server = Server::new_with_config(GapConfig::Peripheral(PeripheralConfig {
-        name: DEVICE_NAME,
-        appearance: &appearance::UNKNOWN,
-    }))
-    .unwrap();
+    let server = SERVER.init(
+        Server::new_with_config(GapConfig::Peripheral(PeripheralConfig {
+            name: DEVICE_NAME,
+            appearance: &appearance::UNKNOWN,
+        }))
+        .unwrap(),
+    );
+
+    spawner.spawn(bluetooth_runner_task(runner).unwrap());
+    spawner.spawn(bluetooth_app_task(peripheral, server).unwrap());
+
+    // join(
+    //     async {
+    //         loop {
+    //             if let Err(e) = runner.run().await {
+    //                 panic!("BLE RUNNER ERROR {}", e);
+    //             }
+    //         }
+    //     },
+    //     async {
+    //         //todo
+    //     },
+    // )
+    // .await;
 
     // GATT (Generic Attribute File) server. This defines how BLE devices exchange data once
     // they're connected. It holds the data and exposes it to other devices.
@@ -47,16 +79,33 @@ pub fn init(spawner: &Spawner, bluetooth: BT<'static>) {
 }
 
 #[embassy_executor::task]
-pub async fn listen_to_bluetooth(bluetooth: BT<'static>) {
-    let transport = BleConnector::new(bluetooth, Default::default()).unwrap();
-    let ble_controller = ExternalController::<_, 20>::new(transport);
-    let mut resources: HostResources<DefaultPacketPool, CONNECTIONS_MAX, L2CAP_CHANNELS_MAX> =
-        HostResources::new();
-
-    let stack = trouble_host::new(ble_controller, &mut resources)
-        .set_random_address(Address::random(BLUETOOTH_DEVICE_ADDRESS));
-
-    let host = stack.build();
-
-    // let host = stack.
+async fn bluetooth_runner_task(
+    mut runner: Runner<'static, ExternalController<BleConnector<'static>, 20>, DefaultPacketPool>,
+) {
+    if let Err(e) = runner.run().await {
+        panic!("BLE RUNNER ERROR {}", e);
+    }
 }
+
+#[embassy_executor::task]
+async fn bluetooth_app_task(
+    mut peripheral: Peripheral<'static, BleController, DefaultPacketPool>,
+    server: &'static Server<'static>,
+) {
+    //todo
+}
+
+// #[embassy_executor::task]
+// pub async fn listen_to_bluetooth(bluetooth: BT<'static>) {
+//     let transport = BleConnector::new(bluetooth, Default::default()).unwrap();
+//     let ble_controller = ExternalController::<_, 20>::new(transport);
+//     let mut resources: HostResources<DefaultPacketPool, CONNECTIONS_MAX, L2CAP_CHANNELS_MAX> =
+//         HostResources::new();
+
+//     let stack = trouble_host::new(ble_controller, &mut resources)
+//         .set_random_address(Address::random(BLUETOOTH_DEVICE_ADDRESS));
+
+//     let host = stack.build();
+
+//     // let host = stack.
+// }
